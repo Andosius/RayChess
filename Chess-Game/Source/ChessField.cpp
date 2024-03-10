1135d8b08e68be322ff800422726237e6175b312
Denis Ahmatovic <37641046+Andosius@users.noreply.github.com>
2024-03-10 15:58:32 +0100

    Add new way to work with colors

--- Chess-Game/Source/ChessField.cpp
+++ Chess-Game/Source/ChessField.cpp
@@ -15,7 +15,6 @@
 
 ChessField::ChessField()
 {
-    FieldColor = WHITE;
     Piece = ' ';
 }
 
@@ -23,4 +22,39 @@ ChessField::ChessField(Color color, char piece)
 {
     FieldColor = color;
     Piece = piece;
+}
+
+void ChessField::AddSpecialColor(Color color)
+{
+    R += color.r;
+    G += color.g;
+    B += color.b;
+    A += color.a;
+    ColorCount += 1;
+
+    Color col = {};
+    col.r = R / ColorCount;
+    col.g = G / ColorCount;
+    col.b = B / ColorCount;
+    col.a = A / ColorCount;
+}
+
+Color ChessField::GetSpecialColor()
+{
+    Color col = {};
+    col.r = R / ColorCount;
+    col.g = G / ColorCount;
+    col.b = B / ColorCount;
+    col.a = A / ColorCount;
+
+    return col;
+}
+
+void ChessField::ResetSpecialColor()
+{
+    R = 0;
+    G = 0;
+    B = 0;
+    A = 0;
+    ColorCount = 0;
 }
--- Chess-Game/Source/Include/ChessField.hpp
+++ Chess-Game/Source/Include/ChessField.hpp
@@ -18,6 +18,17 @@ struct ChessField
     ChessField();
     ChessField(Color color, char piece);
 
-    Color FieldColor;
+    void AddSpecialColor(Color color);
+    Color GetSpecialColor();
+    void ResetSpecialColor();
+
     char Piece;
+    Color FieldColor = WHITE;
+
+    int R = 0;
+    int G = 0;
+    int B = 0;
+    int A = 0;
+
+    int ColorCount = 0;
 };
